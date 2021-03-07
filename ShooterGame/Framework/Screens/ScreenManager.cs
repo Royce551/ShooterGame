12a838e9de254402d8e2bf1e7707d11c19eb5851
Royce551 <poohwie2005@gmail.com>
2021-03-07 12:00:57 -0600

    Add project files.

---
+++ ShooterGame/Framework/Screens/ScreenManager.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShooterGame.Framework.Screens
+{
+    public class ScreenManager
+    {
+        public Stack<Screen> Screens { get; } = new();
+        public bool HasScreens { get => Screens.Count > 0; }
+
+        public void AddScreen(Screen screen)
+        {
+            Screens.Push(screen);
+        }
+
+        public void RemoveScreen()
+        {
+            Screens.Pop();
+        }
+        public void RemoveAllScreens()
+        {
+            while (HasScreens)
+                RemoveScreen();
+
+        }
+        public void ChangeScreen(Screen screen)
+        {
+            RemoveAllScreens();
+            AddScreen(screen);
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            if (!HasScreens) return;
+            Screens.Peek().Update(gameTime);
+        }
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            if (!HasScreens) return;
+            Screens.Peek().Draw(gameTime, spriteBatch);
+        }
+    }
+}
---
+++ ShooterGame/ShooterGame.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.IO;
+
+namespace ShooterGame
+{
+    public class ShooterGame : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        private Texture2D texture;
+        private Vector2 position;
+        public ShooterGame()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            texture = Texture2D.FromFile(GraphicsDevice, "Assets/test.png");
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            var state = Keyboard.GetState();
+            if (state.IsKeyDown(Keys.Left))
+                position.X -= 100 * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (state.IsKeyDown(Keys.Right))
+                position.X += 100 * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(texture, position, Color.White);
+            // TODO: Add your drawing code here
+            _spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
